8a8315e41d7a2b665b6fa96a15ea8eec4db5471c
Jenny Andrea <jennyandrea369@gmail.com>
2017-02-10 14:53:49 +0000

    Tiny turtle

---
+++ script.js
@@ -0,0 +1,16 @@
+ TinyTurtle.apply(window);
+ forward(20);
+ right(90);
+ forward(40);
+ right(90);
+ forward(20);
+ right(90);
+ forward(40);
+ right(90);
+ forward(20);
+ right(30);
+ forward(50)
+ right(130);
+ forward(50);
+
+ 
